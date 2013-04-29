48563ecfd82dd8d6de93580c193cbf24d414c96b
Renata Carreira <recarreira@buzz.local>
2013-04-29 16:17:27 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterSandbox::Application.config.secret_token = '204ad1d802a74344014c23157fad3cbbd9313dde89ed7d15f0c7ee548acea36e63ae518ed0a7815d30f12db6b143e4555ef9f8007e4c746fc9ff81ab664a7630'
